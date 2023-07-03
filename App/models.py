ae8853cb9e7f1efb4f11001c62f1e37d54029d1a
ASHFAQ ISSAK <ashfaq.abdi@gmail.com>
2023-07-03 22:07:55 +0300

    added import for sqlalchemy ans werkzeug

--- App/models.py
+++ App/models.py
@@ -4,11 +4,10 @@ from werkzeug.security import generate_password_hash, check_password_hash
 db = SQLAlchemy()
 
 
-class Fan(db.Model):
-    __tablename__ = "fans"
+class User(db.Model):
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    fan_id = db.Column(db.String(50), unique=True, nullable=False)
-    fan_name = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
